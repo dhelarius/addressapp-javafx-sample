5b9f09475ac2e0e58216a216f7013f5544bebe79
Dario Jiménez <jeffrey.ayala2040@gmail.com>
2022-03-18 14:17:36 -0400

    Add user interaction

--- src/main/java/com/example/address/controller/PersonOverviewController.java
+++ src/main/java/com/example/address/controller/PersonOverviewController.java
@@ -2,10 +2,13 @@ package com.example.address.controller;
 
 import com.example.address.AddressApplication;
 import com.example.address.model.Person;
+import com.example.address.utils.DateUtil;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import org.controlsfx.control.NotificationPane;
 
 /**
  * @author djimenez on 18/3/2022
@@ -51,6 +54,13 @@ public class PersonOverviewController {
         // Initialize the person table with the two columns.
         firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
         lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+
+        // Clear person details.
+        showPersonDetails(null);
+
+        // Listen for selection changes and show the person details when changed.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     /**
@@ -63,5 +73,90 @@ public class PersonOverviewController {
 
         // Add observable list data to the table
         personTable.setItems(addressApplication.getPersonData());
+    }
+
+    /**
+     * Fills all text fields to show details about the person.
+     * If the specified person is null, all text fields are cleared.
+     *
+     * @param person the person or null
+     */
+    private void showPersonDetails(Person person) {
+        // TODO: We need a way to convert the birthday into a String!
+        // birthdayLabel.setText(...);
+        if(person == null) {
+            // Person is null, remove all the text.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+            return;
+        }
+
+        // Fill the labels with info from the person object.
+        firstNameLabel.setText(person.getFirstName());
+        lastNameLabel.setText(person.getLastName());
+        streetLabel.setText(person.getStreet());
+        postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+        cityLabel.setText(person.getCity());
+        birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+    }
+
+    /**
+     * Called when the user clicks the new button. Opens a dialog to edit
+     * details for a new person.
+     */
+    @FXML
+    private void handleNewPerson() {
+        Person tempPerson = new Person();
+        boolean okClicked = addressApplication.showPersonEditDialog(tempPerson);
+        if (okClicked) {
+            addressApplication.getPersonData().add(tempPerson);
+        }
+    }
+
+    /**
+     * Called when the user clicks the edit button. Opens a dialog to edit
+     * details for the selected person.
+     */
+    @FXML
+    private void handleEditPerson() {
+        Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+        if (selectedPerson == null) {
+            // Nothing selected.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+            alert.showAndWait();
+
+            return;
+        }
+
+        boolean okClicked = addressApplication.showPersonEditDialog(selectedPerson);
+        if (okClicked) {
+            showPersonDetails(selectedPerson);
+        }
+    }
+
+    /**
+     * Called when the user clicks on the delete button.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selecteIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selecteIndex < 0) {
+            // Nothing selected.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No person selected");
+            alert.setContentText("Please select a person in the table.");
+            alert.showAndWait();
+            return;
+        }
+
+        personTable.getItems().remove(selecteIndex);
     }
 }
